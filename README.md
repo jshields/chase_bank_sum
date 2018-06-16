b51783c8b7c5e9a7ca7495b679f435827884709b
jshields <shields.h.josh@gmail.com>
2018-06-16 16:59:10 -0500

    add script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # chase_bank_sum
 Silly personal script to add up line items in bank statement CSV
+
+Usage:
+
+    python chase_bank_sum.py /path/to/export.csv
---
+++ chase_bank_sum.py
@@ -0,0 +1,32 @@
+"""
+paranoid script to make sure line items sum to expected total on bank statement
+"""
+import sys
+
+
+def process_line(line):
+    return line.strip().split(',')
+
+
+def amount_from_line(line):
+    """
+    :returns: string `'Amount'` from header row or int amount from regular row
+    """
+    return line[-1]
+
+
+def sum_chase_bank_statement(file_path):
+    with open(file_path) as file_handle:
+        header = file_handle.readline()
+        if amount_from_line(process_line(header)) != 'Amount':
+            raise ValueError('Amount column not in expected position')
+
+        amounts = [
+            float(amount_from_line(process_line(val))) for val
+            in file_handle.readlines()
+        ]
+        return sum(amounts)
+
+
+if __name__ == '__main__':
+    print(sum_chase_bank_statement(sys.argv[1]))
